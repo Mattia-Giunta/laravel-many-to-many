6e787f532e153bc3543eb6b6396dde11d6b8dbc2
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-04-03 12:04:17 +0200

    compilato technologyseeder e effettuato il comando per popolare la tabella technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 use App\Models\Type;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -30,5 +31,10 @@ class Project extends Model
     public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,22 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            "coding",
+            "laravel",
+            "css",
+            "php",
+            "js",
+            "vue",
+            "html",
+        ];
+
+        foreach ($technologies as $element){
+            $new_technology = new Technology();
+            $new_technology->name = $element;
+            $new_technology->slug = Str::slug($new_technology->name);
+            $new_technology->save();
+
+        }
     }
 }
